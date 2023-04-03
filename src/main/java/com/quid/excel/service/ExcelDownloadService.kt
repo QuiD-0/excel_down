1b142d429dd2b5ff72db95a5ba5826a6273b68ea
QuiD-0 <wodnd101@naver.com>
2023-04-03 10:29:50 +0900

    datetime format 적용

--- src/main/java/com/quid/excel/service/ExcelDownloadService.kt
+++ src/main/java/com/quid/excel/service/ExcelDownloadService.kt
@@ -4,6 +4,8 @@ import com.quid.excel.component.Binder
 import com.quid.excel.component.Internationalization
 import org.springframework.stereotype.Component
 import java.net.URLEncoder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.servlet.http.HttpServletResponse
 
 @Component
@@ -13,7 +15,8 @@ class ExcelDownloadService {
         response.contentType = "application/vnd.ms-excel"
         response.characterEncoding = "utf-8"
         val fileNameUtf8 = URLEncoder.encode(name, "UTF-8")
-        response.setHeader("Content-Disposition", "attachment; filename=$fileNameUtf8.xlsx")
+        val now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        response.setHeader("Content-Disposition", "attachment; filename=" + fileNameUtf8 +"_"+now+".xlsx");
         Binder(response.outputStream, name, data, lang).build()
     }
 }
